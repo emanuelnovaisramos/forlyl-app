ea6aef9b069790fbf84986c852c8d3f03b4481dd
Emanuel Novais <emanuelnovais2003@gmail.com>
2025-08-19 16:41:28 -0300

    feat: gantt

--- components/project/gantt/index.tsx
+++ components/project/gantt/index.tsx
@@ -1,17 +1,31 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './styles.scss'
 import 'dhtmlx-gantt/codebase/dhtmlxgantt.css'
 import 'dhtmlx-gantt'
+import { Drawer, DrawerContent } from '@/components/ui/drawer'
+import { FaArrowLeft } from 'react-icons/fa6'
+import { LuCalendar, LuUser } from 'react-icons/lu'
+import { IoIosArrowRoundForward } from 'react-icons/io'
+import { PiSpinnerBold } from 'react-icons/pi'
+import Image from 'next/image'
 
 export const GanttChart = () => {
   const ganttContainer = useRef<HTMLDivElement>(null)
+  const drawerRef = useRef<HTMLDivElement>(null)
+  const [openSidebar, setOpenSidebar] = useState(false)
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const gantt = (window as any).gantt
     if (!ganttContainer.current) return
+
+    gantt.attachEvent('onTaskClick', function () {
+      //const task = gantt.getTask(id)
+      setOpenSidebar(true)
+      return true
+    })
 
     gantt.config.drag_links = false
     gantt.config.drag_move = false
@@ -158,7 +172,7 @@ export const GanttChart = () => {
     gantt.parse({ data: tasks })
 
     const rowHeight = gantt.config.row_height || 30
-    const totalHeight = tasks.length * rowHeight + 51
+    const totalHeight = tasks.length * rowHeight + 52
     ganttContainer.current.style.height = `${totalHeight}px`
 
     return () => {
@@ -166,8 +180,93 @@ export const GanttChart = () => {
     }
   }, [])
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        drawerRef.current &&
+        !drawerRef.current.contains(event.target as Node)
+      ) {
+        setOpenSidebar(false)
+      }
+    }
+
+    if (openSidebar) {
+      document.addEventListener('mousedown', handleClickOutside)
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [openSidebar])
+
   return (
     <div className="w-full">
+      <Drawer
+        open={openSidebar}
+        onOpenChange={setOpenSidebar}
+        direction="right"
+      >
+        <DrawerContent
+          ref={drawerRef}
+          className="flex bg-transparent border-none justify-start items-end h-full"
+        >
+          <div className="flex flex-col w-full p-7.5 gap-7.5 items-start max-w-[700px] bg-white h-full">
+            <div className="flex items-center gap-7.5">
+              <FaArrowLeft
+                size={24}
+                className="cursor-pointer"
+                onClick={() => setOpenSidebar(false)}
+              />
+              <h3 className="text-2xl">Criativos de Captação</h3>
+            </div>
+            <div className="flex gap-5 flex-col">
+              <div className="flex items-center gap-2">
+                <div className="flex items-center min-w-[200px] text-third text-sm gap-4">
+                  <LuCalendar size={18} />
+                  Responsável
+                </div>
+                <p className="text-sm flex items-center gap-1">
+                  30 de novembro de 2024 <IoIosArrowRoundForward size={16} /> 5
+                  de novembro de 2024
+                </p>
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="flex items-center min-w-[200px] text-third text-sm gap-4">
+                  <LuUser size={18} />
+                  Responsável
+                </div>
+                <p className="text-sm">Gustavo Ferreira</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="flex items-center min-w-[200px] text-third text-sm gap-4">
+                  <PiSpinnerBold size={18} />
+                  Status
+                </div>
+                <div className="text-sm bg-done p-1 rounded-md">Feito</div>
+              </div>
+            </div>
+            <div className="border-t w-full border-border-primary">
+              <div className="flex gap-2 items-start py-2.5">
+                <div className="relative gap-2 h-6 w-6 overflow-hidden rounded-full">
+                  <Image
+                    src={'/user-avatar-example.png'}
+                    alt={'user'}
+                    fill
+                    className="object-cover rounded-full"
+                  />
+                </div>
+                <textarea
+                  className="bg-none placeholder:text-four h-auto max-h-[300px] outline-none focus:outline-none rounded-none text-sm border-none p-0 w-full resize-none"
+                  placeholder="Adicionar um comentário"
+                  rows={1}
+                />
+              </div>
+            </div>
+          </div>
+        </DrawerContent>
+      </Drawer>
       <div ref={ganttContainer} className="w-full h-full gantt" />
     </div>
   )
